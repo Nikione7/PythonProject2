cf4982f8b15d8ce16c84fef88337902a410ff5e7
Nikione7 <grandioznovnikilandzello@gmail.com>
2024-11-29 21:47:45 +0300

    Добавил оператора else

--- module_2_2.py
+++ module_2_2.py
@@ -5,5 +5,5 @@ if first == second and first == third and second == third:
     print(3)
 elif first == second or first == third or second == third:
     print(2)
-elif first != second and first != third and second != third:
+else:
     print(0)
